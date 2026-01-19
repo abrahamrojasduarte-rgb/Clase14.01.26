4a9a3c582613ef714884989d4b0a09907b2e2b54
ESCINF <abraham.rojas.duarte@est.una.ac.cr>
2026-01-19 13:52:19 -0600

    listo

--- Horario.cpp
+++ Horario.cpp
@@ -3,3 +3,72 @@
 //
 
 #include "Horario.h"
+
+Horario::Horario() {
+    this->limpiar();
+}
+
+void Horario::agregarClase(int dia, int hora, string nombreClase) {
+    if (dia >= 0 && dia < DIAS && hora >= 0 && hora < HORAS) {
+        m[dia][hora] = nombreClase;
+    }
+}
+
+int Horario::clasesPorDia(int dia) {
+    if (dia < 0 && dia > DIAS) {
+        return 0;
+    }
+    int tot = 0;
+    for (int j = 0; j < HORAS; j++) {
+        if (m[dia][j] == "") {
+            tot++;
+        }
+    }
+    return HORAS - tot;
+}
+
+int Horario::diaMasOcupado() {
+    int maxClases = clasesPorDia(0);
+    int indice = 0;
+
+    for (int dia = 1; dia < DIAS; dia++) {
+        int clases = clasesPorDia(dia);
+
+        if (clases > maxClases) {
+            maxClases = clases;
+            indice = dia;
+        }
+    }
+
+    return indice;
+}
+
+
+bool Horario::estaLibre(int dia, int hora) {
+    bool estaLibre = false;
+    if (dia >= 0 && dia < DIAS && hora >= 0 && hora < HORAS) {
+        if (m[dia][hora] == "") {
+            estaLibre = true;
+        }
+    }
+    return estaLibre;
+}
+
+void Horario::limpiar() {
+    for (int i = 0; i < DIAS; i++) {
+        for (int j = 0; j < HORAS; j++) {
+            m[i][j] = " ";
+        }
+    }
+}
+
+string Horario::toString() {
+    stringstream ss;
+    for (int i = 0; i < DIAS; i++) {
+        for (int j = 0; j < HORAS; j++) {
+            ss<<m[i][j];
+        }
+        ss<<endl;
+    }
+    return ss.str();
+}
--- Horario.h
+++ Horario.h
@@ -4,9 +4,23 @@
 
 #ifndef MAIN_CPP_HORARIO_H
 #define MAIN_CPP_HORARIO_H
-
+#include <iostream>
+#include <sstream>
+using namespace std;
 
 class Horario {
+private:
+    static const int DIAS = 7;
+    static const int HORAS = 8;
+    string m[DIAS][HORAS];
+    public:
+    Horario();
+    void agregarClase(int dia, int hora, string nombreClase);
+    int clasesPorDia(int dia);
+    int diaMasOcupado();
+    bool estaLibre(int dia, int hora);
+    void limpiar();
+    string toString();
 };
 
 
